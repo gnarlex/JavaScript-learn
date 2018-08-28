562e48539a47755ec53f9e5a9ee341dd5c09e0bf
fairo <fairoliur@gmail.com>
2018-08-28 23:02:12 +0800

    jquery plug

--- JsPlug/jquery-superStar/jquery-superStar.1.0.js
+++ JsPlug/jquery-superStar/jquery-superStar.1.0.js
@@ -5,16 +5,34 @@
 	function(win, $, doc) {
 		$.fn.superStar = function(options) {
 			var
-			default = {
-				//默认星星的个数
-				//打完分的回调方法
-				//样式
-				cssClass:"superStar"
-			}
+				setting = {
+					//默认星星的个数
+					size: 5,
+					//打完分的回调方法
+					callback: null,
+					//样式
+					cssClass: "superStar"
+				}
 			//覆盖参数
 			$.extend(
-				default, options);
-				//输出星星
+				setting, options);
+			//给容器添加样式
+			this.toggleClass(setting.cssClass);
+			//输出星星 
+			for(var i = 0; i < setting.size; i++) {
+				var star = $("<a/>").addClass("starItem").addClass("empty").appendTo(this);
+				star.mouseover(function() {
+					var parent = star.closest("." + setting.cssClass);
+					parent.find(".starItem").removeClass("full");
+					var index = parent.find(".starItem").index($(this));
+					parent.find(".starItem:lt(" + (index + 1) + ")").addClass("full");
+				});
+				star.mouseleave(function() {
+					var parent = star.closest("." + setting.cssClass);
+					parent.find(".starItem").removeClass("full"); 
+				});
+			}
+			return this;
 		}
 	}
 );
